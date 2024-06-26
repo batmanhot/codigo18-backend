dfb6a91c867d2e22b4d7f8481314dc370e7ee36a
JHON PONTE <108916708+batmanhot@users.noreply.github.com>
2024-06-26 00:56:10 -0500

    semana7

--- semana-6/todolist-api/todo_list_api/users/serializers.py
+++ semana-6/todolist-api/todo_list_api/users/serializers.py
@@ -1,11 +1,13 @@
-from .models import User
 from rest_framework.serializers import ModelSerializer, Serializer, EmailField, CharField
+from .models import User
 from django.contrib.auth.hashers import make_password
+
 
 # La mision de el, es verificar que los campos que estamos recibiendo solo sean el email y el password
 class UserLoginSerializer(Serializer):
     email = EmailField(required=True)
     password = CharField(required=True)
+
 
 class UserSerializer(ModelSerializer):
     class Meta:
@@ -14,5 +16,6 @@ class UserSerializer(ModelSerializer):
 
     def create(self, validated_data):
         if 'password' in validated_data:
-            validated_data['password'] = make_password(validated_data['password'])
+            validated_data['password'] = make_password(
+                validated_data['password'])
         return super().create(validated_data)
--- semana-6/todolist-api/todo_list_api/users/urls.py
+++ semana-6/todolist-api/todo_list_api/users/urls.py
@@ -1,14 +1,3 @@
-# from rest_framework.routers import DefaultRouter
-# from .views import UserViewSet
-
-# router = DefaultRouter()
-
-# # Se tiene que registrar las rutas
-
-# router.register (r'users', UserViewSet)
-
-# urlpatterns = router.urls
-
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, AuthenticationView
 from django.urls import path
